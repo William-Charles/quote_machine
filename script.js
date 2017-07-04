96aa74319d1683d41f2dd746182ff8eae2506d05
William Whitworth <william10charles@gmail.com>
2017-07-03 23:47:58 -0600

    js only request work

--- script.js
+++ script.js
@@ -2,9 +2,35 @@ document.addEventListener("DOMContentLoaded", function() {
   let quoteButton = document.getElementById("quote-button");
   let quote = document.getElementById("quote");
   let author = document.getElementById("author");
+  let tweetBtn = document.getElementById("tweet-button");
 
-  quoteButton.addEventListener("click", e => {
-    e.preventDefault();
+  tweetBtn.addEventListener("click", function() {
+    var tweet =
+      "https://twitter.com/intent/tweet?text=" +
+      encodeURIComponent("hello" + " -" + "htmlN");
+    window.open(tweet);
+  });
+
+  quoteButton.addEventListener("click", event => {
+    event.preventDefault();
+
+    let headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append("Access-Control-Allow-Origin", "*");
+
+    let init = {
+      method: "GET",
+      headers: headers,
+      mode: "no-cors"
+    };
+    fetch("http://quotesondesign.com/api/3.0/api-3.0.json?callback=?", init)
+      .then(response => {
+        console.log(response);
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+      });
     quote.innerHTML = "fart";
   });
 });
